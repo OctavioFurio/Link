5b39ec71f170cafb63ae8a8e1c64355971537e32
MuriloMGrosso <murilogrosso@hotmail.com>
2026-05-17 11:08:05 -0300

    Requirements update

--- backend/firebase_client.py
+++ backend/firebase_client.py
@@ -6,7 +6,6 @@ def _init():
     if firebase_admin._apps:
         return
     # set env's FIREBASE_SERVICE_ACCOUNT as JSON
-    # TODO: Actually figure out how to do this
     sa = json.loads(os.environ["FIREBASE_SERVICE_ACCOUNT"])
     cred = credentials.Certificate(sa)
     firebase_admin.initialize_app(cred)

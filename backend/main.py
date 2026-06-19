7c901f72d9c4795249f78c4ac32a68ceaa61c17f
Murilo M. Grosso <murilogrosso@hotmail.com>
2026-06-19 18:39:04 -0300

    Default middleware

--- backend/main.py
+++ backend/main.py
@@ -6,9 +6,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://octaviofurio.github.io"],
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Content-Type"],
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(rec.router)
